53854319e8d450d677f604dd4930230bcad8f4c3
jihadul4kbar <jihadul4kbar>
2024-07-11 17:13:24 +0800

    php native

---
+++ mvc-native/app/config/config.php
@@ -0,0 +1,8 @@
+<?php
+define('BASEURL', 'http://localhost/mvc-native/');
+
+// Database
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'belajarphp');
